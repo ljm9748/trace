22d071665090613d49936cb6301a1ed38eb70a1e
JunhoBae <junho91@naver.com>
2019-07-17 22:07:24 +0900

    3rd

--- traceproject/traceproject/urls.py
+++ traceproject/traceproject/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+import accounts.views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',accounts.views.home,name='home'),
 ]
